fd4a23cea647ddd55d62cf70fa91013e58c213ea
YiweiChen1 <umichey@gmail.com>
2020-07-06 12:38:44 -0600

    Added new class Timer (entity) and class message (entity) which will be used in ConfirmTempTradeTransaction (use case). message is the tool that will be used to alert an admin user should an item that partook in Temp Trade ever have a time that is not positive since Timer will be a countdown device

---
+++ phase1/src/Message.java
@@ -0,0 +1,11 @@
+/**
+ * Use Case of Temporary Trade; closes it.
+ *
+ * @author Yiwei Chen
+ * @version 1.0
+ * @since 2020-07-06
+ * last modified 2020-07-06
+ */
+
+public class Message {
+}
---
+++ phase1/src/Timer.java
@@ -0,0 +1,11 @@
+/**
+ * A timer used in by ConfirmTempTradeTransaction; entity class.
+ *
+ * @author Yiwei Chen
+ * @version 1.0
+ * @since 2020-07-06
+ * last modified 2020-07-06
+ */
+
+public class Timer {
+}
