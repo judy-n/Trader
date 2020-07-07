5812e9fad6fff7a57ec53cc65465eb38d60f1270
Allistari <angezhangn@gmail.com>
2020-07-06 22:27:49 -0400

    implemented last two options in the NormalDashboard

--- phase1/src/CompletedTradesViewer.java
+++ phase1/src/CompletedTradesViewer.java
@@ -21,22 +21,35 @@ public class CompletedTradesViewer {
 
 
     public CompletedTradesViewer (NormalUser user, ItemManager im, UserManager um, TradeManager tm) {
+        //this doesn't need SystemPresent since it is a presenter
         currentUser = user;
         itemManager = im;
         userManager = um;
         tradeManager = tm;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        SystemPresenter sp = new SystemPresenter();
-        ArrayList<Trade> completedTrades = tm.getCompletedTrades(currentUser);
+    }
+
+    public void viewCompletedTrades(){
+        ArrayList<Trade> completedTrades = tradeManager.getCompletedTrades(currentUser);
 
         System.out.println("Here are all your completed trades: ");
         for(Trade t : completedTrades){
             //a toString method for trade will be added later
             System.out.println(t);
         }
-
         close();
     }
+
+    public void viewFrequentTrader(){
+        String [] frequentTrader = tradeManager.getFrequentTradePartners(currentUser);
+        System.out.println("Here are your top most frequent trade partners:");
+        for(int i = 0; i< frequentTrader.length; i++){
+            System.out.println((i+1)+". "+ frequentTrader[i]);
+        }
+        close();
+    }
+
+
+
     public void close(){
         new NormalDashboard(currentUser, itemManager, userManager, tradeManager);
     }
