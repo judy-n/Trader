368566a168ee5f5a9ab4a49534950eb2c1448901
Liam <liam.huff@mail.utoronto.ca>
2020-07-11 11:22:38 -0400

    Added javadoc to method in InventoryEditor

--- phase1/src/InventoryEditor.java
+++ phase1/src/InventoryEditor.java
@@ -133,6 +133,9 @@ public class InventoryEditor {
         }
     }
 
+    /**
+     * Closes inventory editing, returning program back to the dashboard
+     */
     private void close() {
         new NormalDashboard(currentUser, itemManager, userManager, tradeManager);
     }
