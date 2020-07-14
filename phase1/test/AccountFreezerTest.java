518fa065859afb2abc98d4c5c57d5a2a97dcd710
mehtaku6 <kushagra.mehta@mail.utoronto.ca>
2020-07-13 21:41:14 -0400

    did AccountFreezerTest

--- phase1/test/AccountFreezerTest.java
+++ phase1/test/AccountFreezerTest.java
@@ -9,6 +9,22 @@ public class AccountFreezerTest {
         assertEquals(false, user1.getIsFrozen());
     }
 
+    @Test
+    public void testUserFrozen(){
+        user1.freeze();
+        assertEquals(true, user1.getIsFrozen());
+    }
 
+    // testing if active again after freezing above^
+    @Test
+    public void testUserReActive(){
+        user1.unfreeze();
+        assertEquals(false, user1.getIsFrozen());
+    }
 
+    @Test
+    public void testUserReFrozen(){
+        user1.freeze();
+        assertEquals(true, user1.getIsFrozen());
+    }
 }
