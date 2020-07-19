22ee1f9725ea2a9bd1e17f04296d8b575f00e684
Judy Naamani <judy.naamani@mail.utoronto.ca>
2020-07-19 16:35:30 -0400

    Created a DemoUser

---
+++ phase2/src/DemoUser.java
@@ -0,0 +1,17 @@
+/**
+ * Represents a demo user.
+ * Demo users can look at items available for trade but can't initiate trades.
+ * @author Judy Naamani
+ * @version 1.0
+ * @since 2020-07-19
+ * last modified 2020-07-19
+ */
+
+public class DemoUser extends User {
+
+    public DemoUser(){
+        super("demo", "demo@demo.com", "123");
+
+    }
+
+}
